66c8b3aa99c099e5fdb9a9c4ace39a3fad9dd8fb
celzoo <celzoaveirofilho@gmail.com>
2022-03-07 17:02:36 -0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+	
+		char[] array = new char[5];
+		array[0] = 'C';
+		array[1] = 'e';
+		array[2] = 'l';
+		array[3] = 'z';
+		array[4] = 'o';
+		int i;
+		for(i=4;i>=0;i--) {;
+		System.out.print(array[i]);
+		}
+		
+	}
+
+}
